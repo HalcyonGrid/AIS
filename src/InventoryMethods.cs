76d889fd0d03445c03a75a53c37c3e7ca0519478
Paul / Jim - Appurist <dev@appurist.com>
2017-10-11 02:35:28 -0300

    ok Cassandra... let's start putting the pieces together.

---
+++ src/InventoryMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenMetaverse;
+
+namespace AIS
+{
+    class InventoryMethods
+    {
+        private UUID _Id;
+
+    }
+}
